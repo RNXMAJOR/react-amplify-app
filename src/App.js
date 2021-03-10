9d688f63c78f3a1d94e9c96f53183107514fa635
RNXMAJOR <oyimaaaron@gmail.com>
2021-03-10 17:28:10 -0600

    html changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
+import image from './GabonFlag.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Amplify</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="form-signin">
+      <form>
+        <img className="mb-4" src={image} alt="" width="70" height="70">
+        <h1 >Please sign in</h1>
+        <label for="inputEmail" className="visually-hidden">Email address</label>
+        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" className="visually-hidden">Password</label>
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required>
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2021</p>
+      </form>
+    </main>
+    
   );
 }
 
